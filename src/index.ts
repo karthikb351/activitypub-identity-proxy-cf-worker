d17fe62c19b0ff921dfe615760c2f96f1f62b6fc
Karthik Balakrishnan <karthikb351@gmail.com>
2023-12-05 08:38:02 +0000

    add karthikb351.com

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,24 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-}
-
-const proxyMap: Record<string, string|undefined> = {
-	'reroll.in': 'social.reroll.in',
-	'karthikbalakrishnan.com': 'fedi.karthikbalakrishnan.com',
-}
+const proxyMap: Record<string, string | undefined> = {
+  'reroll.in': 'social.reroll.in',
+  'karthikbalakrishnan.com': 'fedi.karthikbalakrishnan.com',
+  'karthikb351.com': 'fedi.karthikb351.com',
+};
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		const url = new URL(request.url);
-		const newHostname = proxyMap[url.hostname]
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+    const newHostname = proxyMap[url.hostname];
 
-		if(!newHostname) {
-			return new Response("Sorry, this hostname is invalid. Please check the proxyMap config on your worker.", {
-				status: 404,
-			  });
-		}
+    if (!newHostname) {
+      return new Response(
+        'Sorry, this hostname is invalid. Please check the proxyMap config on your worker.',
+        {
+          status: 404,
+        }
+      );
+    }
 
-		url.hostname = newHostname;
-		return await fetch(url.toString(), request);
-	},
+    url.hostname = newHostname;
+    return await fetch(url.toString(), request);
+  },
 };
